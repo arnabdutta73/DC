2d897e7b25a00b08d642c508c683e01c106d08c2
arnab <arnabdutta73@gmail.com>
2017-12-08 17:34:36 +0530

    added file description

--- constants.py
+++ constants.py
@@ -1,3 +1,7 @@
+"""
+Constants File
+:Data DICTIONARY
+"""
 DICTIONARY = ['aisle', 'alpha', 'apartment', 'area', 'bazar', 'beta', 'building', 'block', 'cantonment', 'circle',
               'chowk', 'colony', 'complex', 'district', 'department', 'enclave', 'estate', 'extension', 'flat', 'floor',
               'government', 'house', 'industrial', 'layout', 'limited', 'market', 'mohalla', 'nagar', 'palika', 'phase',
