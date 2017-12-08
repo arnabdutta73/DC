f79b91435cd594aa36c3745311f3db35d804f287
arnab <arnabdutta73@gmail.com>
2017-12-08 17:20:32 +0530

    first commit

---
+++ constants.py
@@ -0,0 +1,4 @@
+DICTIONARY = ['aisle', 'alpha', 'apartment', 'area', 'bazar', 'beta', 'building', 'block', 'cantonment', 'circle',
+              'chowk', 'colony', 'complex', 'district', 'department', 'enclave', 'estate', 'extension', 'flat', 'floor',
+              'government', 'house', 'industrial', 'layout', 'limited', 'market', 'mohalla', 'nagar', 'palika', 'phase',
+              'pocket', 'quarter', 'road', 'room', 'sector', 'society', 'square', 'vihar']
